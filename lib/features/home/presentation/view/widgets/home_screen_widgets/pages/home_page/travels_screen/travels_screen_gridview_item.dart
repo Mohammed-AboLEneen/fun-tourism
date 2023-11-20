415294129d2bd3008477ddecd53a0267e0387f5f
mohme <mohmedhty1@gmail.com>
2023-11-21 00:29:30 +0200

    create when click on gridview item of travels screen effects on image and some animations (completed now (:).

--- lib/features/home/presentation/view/widgets/home_screen_widgets/pages/home_page/travels_screen/travels_screen_gridview_item.dart
+++ lib/features/home/presentation/view/widgets/home_screen_widgets/pages/home_page/travels_screen/travels_screen_gridview_item.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fun_adventure/cores/utils/color_degree.dart';
 import 'package:fun_adventure/cores/utils/screen_dimentions.dart';
-import 'package:fun_adventure/features/home/presentation/view/widgets/custom_textbutton.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../../../custom_textbutton.dart';
 import 'gridview_border_radius.dart';
 
 class TravelsScreenGridViewItem extends StatefulWidget {
@@ -110,38 +110,44 @@ class _TravelsScreenGridViewItemState extends State<TravelsScreenGridViewItem>
                 SizedBox(
                   height: 10.h,
                 ),
-                if (apperButtonContainer)
-                  TweenAnimationBuilder(
-                    duration: const Duration(milliseconds: 400),
-                    tween: Tween<Offset>(
-                        begin: const Offset(0, -5), end: const Offset(0, 0)),
-                    builder:
-                        (BuildContext context, Offset value, Widget? child) {
-                      return Transform.translate(
-                        offset: value,
-                        child: AnimatedContainer(
-                          onEnd: () {
-                            apperButtonContainer = false;
-                          },
+                FutureBuilder(
+                    future: Future.delayed(const Duration(milliseconds: 800)),
+                    builder: (_, snap) {
+                      if (snap.connectionState == ConnectionState.waiting) {
+                        return Container();
+                      } else {
+                        return TweenAnimationBuilder(
                           duration: const Duration(milliseconds: 400),
-                          height: context.height *
-                              (((5 - (value.dy * -1)) / 5) * .045).h,
-                          child: CustomTextButton(
-                            text: 'More Info',
-                            buttonColor:
-                                const Color(0xff313745).withOpacity(.8),
-                            onPressed: () {},
-                            topLeft: 10,
-                            topRight: 10,
-                            bottomRight: 10,
-                            bottomLeft: 10,
-                            textSize: 15.sp,
-                            buttonColorLightness: .3,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
+                          tween: Tween<Offset>(
+                              begin: const Offset(0, -5),
+                              end: const Offset(0, 0)),
+                          builder: (BuildContext context, Offset value,
+                              Widget? child) {
+                            return Transform.translate(
+                              offset: value,
+                              child: AnimatedContainer(
+                                onEnd: () {},
+                                duration: const Duration(milliseconds: 400),
+                                height: context.height *
+                                    (((5 - (value.dy * -1)) / 5) * .045).h,
+                                child: CustomTextButton(
+                                  text: 'More Info',
+                                  buttonColor:
+                                      const Color(0xff313745).withOpacity(.8),
+                                  onPressed: () {},
+                                  topLeft: 10,
+                                  topRight: 10,
+                                  bottomRight: 10,
+                                  bottomLeft: 10,
+                                  textSize: 15.sp,
+                                  buttonColorLightness: .3,
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      }
+                    })
               ],
             ),
           )
